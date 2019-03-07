464936ffa8c991af3c8a064530b942bd03dfa7a2
Sergio San Pedro <sergio.sanpedro@me.com>
2019-03-07 12:06:52 -1000

    Initial Commit

---
+++ Assignment6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment6
+//
+//  Created by Sergio San Pedro on 3/7/19.
+//  Copyright © 2019 Sergio San Pedro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
